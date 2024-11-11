1d4f6500b82045f6a3e9165af0c2bb6621e686d3
Abhay Bavistale <122510007+abhay45451@users.noreply.github.com>
2024-11-11 21:11:30 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String username, String image, String tag){
-  withCredentials([UsernamePassword(credentialsId: "docker", passwordVariable: 'dockerpass', usernameVariable: 'dockeruser')]){
-    sh ' docker login -u ${dockeruser} -p ${dockerpass}'
-}
-  sh 'docker push ${username}/${image}:${tag}'
+def call(String username, String image, String tag) {
+    withCredentials([usernamePassword(credentialsId: "docker", passwordVariable: 'dockerpass', usernameVariable: 'dockeruser')]) {
+        sh "docker login -u ${dockeruser} -p ${dockerpass}"
+    }
+    sh "docker push ${username}/${image}:${tag}"
 }
